f9d50e57a9a2180ce3586ba2abea63ea11dcde2c
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-01-28 10:54:04 +0100

    Calculate and render percent of posts with cat

--- src/Stats/StatsCategories.js
+++ src/Stats/StatsCategories.js
@@ -1,9 +1,13 @@
 import BlogpostCategory from '../Blogposts/BlogpostCategory.js';
 import { blogpostCategories } from '../constants/constants';
-import { getCategoriesAsArray } from '../services/util.service';
-import { getNumberOfPostsWithCategory } from '../services/posts.service.js';
+import { getNumberOfPosts, getNumberOfPostsWithCategory } from '../services/posts.service.js';
+import { getCategoriesAsArray, displayPercent } from '../services/util.service';
 
 function StatsCategories() {
+
+    const getPercent = (totalNumberOfPosts, numberOfPostsWithCategory) => {
+        return displayPercent(numberOfPostsWithCategory / totalNumberOfPosts);
+    }
 
     return (
         <>
@@ -28,6 +32,18 @@ function StatsCategories() {
                 <div className="col-md-6">
                     <div className="alert alert-light">
                         <h3 className="h5">Percent of posts with category</h3>
+                        <ol className="list-unstyled m-0">
+                        {
+                                getCategoriesAsArray(blogpostCategories).map(c => (
+                                    <li key={c}>
+                                        <span className="row">
+                                            <span className="col col-lg-4"><BlogpostCategory category={c} makeResponsive={false} /></span>
+                                            <span className="col">{getPercent(getNumberOfPosts(), getNumberOfPostsWithCategory(c))}</span>
+                                        </span>
+                                    </li>
+                                ))
+                            }
+                        </ol>
                     </div>
                 </div>
             </div>
