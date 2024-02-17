20ad898b873713d7f1793dca1acd4f27002b5fa3
henrikmathiesen <henrik_mathiesen@hotmail.com>
2024-02-17 13:04:51 +0100

    static quotes

--- src/services/quotable.service.js
+++ src/services/quotable.service.js
@@ -2,15 +2,66 @@
 
 import { randomIntFromInterval } from './util.service';
 
-const relevantCategories = [
-    'Business', 
-    'Change', 
-    'Character', 
-    'Courage', 
-    'Creativity', 
-    'Education', 
-    'Ethics', 
-    'Failure', 
+const staticQuotes = [
+    {
+        content: 'Let things go well.',
+        author: 'Mark'
+    },
+    {
+        content: 'Be unreasonable.',
+        author: 'Mark'
+    },
+    {
+        content: 'It is just a ghost house.',
+        author: 'Shanon'
+    },
+    {
+        content: 'Warrior attitude.',
+        author: 'Shanon'
+    },
+    {
+        content: 'The parachute will open.',
+        author: 'Alli'
+    },
+    {
+        content: 'Stop Ruminating.',
+        author: 'Alli'
+    },
+    {
+        content: 'It is just a water pistol.',
+        author: 'Katie'
+    },
+    {
+        content: 'You have to accept uncertainty.',
+        author: 'Katie'
+    },
+    {
+        content: 'The universe doesnt owe you anything.',
+        author: 'Albert'
+    },
+    {
+        content: 'Drop your god like demands.',
+        author: 'Albert'
+    },
+    {
+        content: 'Want what you dont want.',
+        author: 'Reid'
+    },
+    {
+        content: 'When we are no longer able to change a situation, we are challenged to change ourselves.',
+        author: 'Victor Frankl'
+    }
+];
+
+const relevantCategoriesForHttpCall = [
+    'Business',
+    'Change',
+    'Character',
+    'Courage',
+    'Creativity',
+    'Education',
+    'Ethics',
+    'Failure',
     'Faith',
     'Famous Quotes',
     'Freedom',
@@ -31,24 +82,40 @@ const relevantCategories = [
     'Wisdom'
 ];
 
-function getRandomCategory() {
+function getRandomCategoryForHttpCall() {
 
     const catFirstIndex = 0;
-    const catLastIndex = relevantCategories.length - 1;
+    const catLastIndex = relevantCategoriesForHttpCall.length - 1;
 
     const randomIndex = randomIntFromInterval(catFirstIndex, catLastIndex);
 
-    return relevantCategories[randomIndex];
+    return relevantCategoriesForHttpCall[randomIndex];
+}
+
+function getRandomStaticQuote() {
+    const quoteFirstIndex = 0;
+    const quoteLastIndex = staticQuotes.length - 1;
+
+    const randomIndex = randomIntFromInterval(quoteFirstIndex, quoteLastIndex);
+
+    return staticQuotes[randomIndex];
 }
 
 async function getRandomQuote() {
 
-    const category = getRandomCategory().toLowerCase();
-    const url = `https://api.quotable.io/quotes/random?tags=${category}`;
+    const isZeroOrOne = randomIntFromInterval(0, 1);
 
-    const response = await fetch(url);
-    return response.json();
+    if (isZeroOrOne === 0) {
+        const quote = [getRandomStaticQuote()]; // in an array, to match http response, so logic in caller is the same
+        function resolveQuote() { return new Promise((resolve) => { resolve(quote) }); }
+        return await resolveQuote();
+    } else {
+        const category = getRandomCategoryForHttpCall().toLowerCase();
+        const url = `https://api.quotable.io/quotes/random?tags=${category}`;
 
+        const response = await fetch(url);
+        return response.json();
+    }
 }
 
 export {
